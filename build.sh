6713fe20eaeb0bffc7a7b31ebc1687a0240d3ebd
Eirik Årdal <eaardal@outlook.com>
2018-02-25 19:03:51 +0100

    Moved app

--- build.sh
+++
@@ -1,2 +0,0 @@
-cd movie-posters
-npm run build
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "typeface-roboto";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+import "./index.css";
+
+// This is the app's entry point (the "static void main" equivalent of other languages).
+// You'll find the "root" element in "public/index.html"
+ReactDOM.render(<App />, document.getElementById("root"));
